b9d77094180480ea492d0773501afb7be5d87e52
Dylan <dylanhxd@outlook.com>
2021-02-04 20:27:53 -0800

    create repo

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dto/PurchaseResponse.java
@@ -0,0 +1,11 @@
+package com.luv2code.ecommerce.dto;
+
+// use this class to send back a Java object as JSON
+
+import lombok.Data;
+
+@Data
+public class PurchaseResponse {
+
+    private final String orderTrackingNumber;
+}
